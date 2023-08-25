d3f8d01bbf42c48d9b57bafe9185385b8736e0b7
Paulina Laskowska <paulinal@hawaii.edu>
2023-08-25 17:17:02 -0600

    add active feature to navbar

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 window.onload = function () {
     document.body.className += "loaded";
 }
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 110;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= sectionTop) {
+            current = section.getAttribute('id');
+        }
+    })
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.classList.contains(current)) {
+            link.classList.add('active');
+        }
+    })
+})
